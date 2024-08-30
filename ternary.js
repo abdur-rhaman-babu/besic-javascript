b8de336b3fb4b2db0499320a868914c11304d2eb
abdur rhaman babu <abdurrahmanbab74@gamil.com>
2024-08-30 16:10:59 +0600

    ternary operator

---
+++ ternary.js
@@ -0,0 +1,15 @@
+let num1 = 50;
+let num2 = 40;
+let result;
+
+// ternary operator
+result = num1 > num2 ? num1 * 2 : num1 + num2;
+// console.log(result)
+
+// if, else
+if(num1 > num2){
+    result = num1 * 2
+}else{
+    result = num1 + num2
+}
+console.log(result)
