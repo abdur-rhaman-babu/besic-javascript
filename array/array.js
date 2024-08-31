3b030ae5d6f1746ab449f1a38b167e0d8ea49863
abdur rhaman babu <abdurrahmanbab74@gamil.com>
2024-08-31 09:41:18 +0600

    practice task array done

---
+++ array/array.js
@@ -0,0 +1,26 @@
+const fruits = ['Mango', 'Apple', 'Banana', 'Lemon', 'Lici']
+// console.log(fruits[3])
+fruits[2] = 'Jambura';
+// console.log(fruits)
+
+
+const destinations = ['Coxs bazar', 'Kuakata', 'Sundarban']
+destinations.push('Sajek')
+// console.log(destinations)
+destinations.pop()
+// console.log(destinations)
+
+
+const books = ['Bangla', 'English', 'Math', 'Islaim stadies', 'social work']
+// console.log(books.includes('javascript book'))
+
+const numbers = [1,2,3,4,5,6,7,8,9,10]
+const num = 17;
+// console.log(Array.isArray(numbers))
+// console.log(Array.isArray(num))
+
+const arr1 = [10,20,30,40,50]
+const arr2 = [60,70,80,90,100]
+console.log(arr1.concat(arr2))
+console.log(arr1)
+console.log(arr2)
