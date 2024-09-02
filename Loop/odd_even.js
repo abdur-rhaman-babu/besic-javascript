146654210d399afa67f15532471950defeae6783
abdur rhaman babu <abdurrahmanbab74@gamil.com>
2024-09-02 09:51:47 +0600

    odd and even

---
+++ Loop/odd_even.js
@@ -0,0 +1,20 @@
+// Find all the odd numbers from 61 to 100.
+
+// let num = 61;
+// while(num <= 100){
+//     if( num%2 === 1){
+//         console.log('odd number', num)
+//     }
+//     num++
+// }
+
+// Find all the even numbers from 78 to 98.
+
+let even = 78;
+while(even <= 98){
+    if( even%2 === 0){
+        console.log('odd number', even)
+    }
+    even++
+}
+
