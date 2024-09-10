d0bee9aa76858f94ead2fe0beb9a38543806a27c
abdur rhaman babu <abdurrahmanbab74@gamil.com>
2024-09-10 09:38:20 +0600

    check name

--- problem-2/nameCheck.js
+++ problem-2/nameCheck.js
@@ -1,6 +1,6 @@
 // name check
 const letters = 'aeiouyw';
-const name = [1,2,3,45];
+const name = 'Hero';
 function checkName(name, letters){
     if ( typeof name !== 'string'){
         return 'invalid input'
@@ -12,7 +12,29 @@ function checkName(name, letters){
         return 'bad name'
     }
    }
-  
 }
 const result = checkName(name, letters)
-console.log(result)
+// console.log(result)
+
+const vowels = ['a','e','i','o','u','y','w']
+const sureName = 'Heloo'
+
+function checkName2(name,letters){
+    
+    if ( typeof name !== 'string'){
+        return 'Invalid Input'
+    }
+
+    const names = name.slice(-1).toLowerCase()
+
+    let result = false;
+    for ( const letter of letters ){
+        if( names === letter){
+            result = true;
+        }
+    }
+    return result ? 'Good Name' : 'Bad Name'
+}
+
+const result1 = checkName2(sureName, vowels)
+// console.log(result1)
