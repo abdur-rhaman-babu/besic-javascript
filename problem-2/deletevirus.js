29fe8bd45d9b1de0ac0fdbfd76021580ca37223c
abdur rhaman babu <abdurrahmanbab74@gamil.com>
2024-09-10 16:32:52 +0600

    password

--- problem-2/deletevirus.js
+++ problem-2/deletevirus.js
@@ -1,5 +1,6 @@
 // deletevirus
 const arr = [0,1,10,null,undefined,NaN,-10,true, 55, 60, '', 'hello']
+const arr1 = ['124', null, NaN, {num:2}]
 const string = 'hoiweruwjrekl'
 
 function deleteVirus(numbers){
---
+++ problem-2/password.js
@@ -0,0 +1,46 @@
+// create strong password
+const password = {
+    name:null,
+    dobyear:null,
+    website:null
+}
+
+function dynamicPassword(pass, name, year,web){
+    pass.name = name;
+    pass.dobyear = year;
+    pass.website = web;
+    
+    let  sPassword = ''
+    for ( const p in pass ){
+        sPassword += (pass[p] + '#')
+    }
+    const result = sPassword.slice(0,sPassword.length-1).split('#').reverse().join('#').toLowerCase().charAt(0).toUpperCase() + sPassword.slice(1);
+    return result;
+}
+
+const greatePassword = dynamicPassword(password, 'Abusaid', 1999, 'google' )
+// console.log(greatePassword)
+
+// greate password
+
+const myInfo = {
+    name:'Hasan',
+    birthYear:2005,
+    website:'google'
+}
+function greatePasswords(myInfo, name, year, web){
+    const byear = year.toString();
+    myInfo.name = name;
+    myInfo.birthYear = byear;
+    myInfo.website = web;
+    if ( (byear.length < 4 || byear.length > 4) || ((name === undefined || web === undefined || year === undefined))){
+        console.log('Invalid')
+    }
+    else{
+        const firstLetter = myInfo.website[0].toUpperCase() + myInfo.website.slice(1)
+        const password = firstLetter + '#' + myInfo.name.toLowerCase() + '@' + myInfo.birthYear;
+        console.log(password)
+    }
+}
+
+greatePasswords(myInfo, 'Babul', 1999, 'google') 
